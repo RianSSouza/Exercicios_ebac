b9b5ceb3cdfacca83c24a9f85c41102d5901b742
Rian <rianssouza02@gmail.com>
2025-05-18 17:14:18 -0300

    Commit do exercicio html com js

---
+++ main.js
@@ -0,0 +1,30 @@
+const form = document.getElementById('form-validacao');
+const numA = document.getElementById('num-a');
+const numB = document.getElementById('num-b');
+const campoError = document.querySelector('.error-message');
+const campoSuccess = document.querySelector('.success-message');
+
+function validaNumero(numeroA, numeroB){
+    return numeroB > numeroA
+
+}
+
+form.addEventListener('submit', function(e){
+    let formValido = false;
+
+    e.preventDefault();
+
+    formValido = validaNumero(numA.value, numB.value);
+
+    if (formValido) {
+        numA.style.border = '';
+        campoError.style.display = 'none';
+     
+    campoSuccess.style.display = 'block';
+    }else{
+        numA.style.border = '1px solid red';
+        campoError.style.display = 'block';
+    }
+});
+
+
