eda3ccd0a5eda804bfdbde952382d94e8680cfb7
Rian <rianssouza02@gmail.com>
2025-10-30 20:29:00 -0300

    Enviando atividadde gulp

---
+++ source/scripts/calculadora.js
@@ -0,0 +1,6 @@
+const {soma, subtracao, multiplicacao, divisao} = require('./operacoes');
+
+console.log(soma(15,20));
+console.log(subtracao(15,20));
+console.log(multiplicacao(15,20));
+console.log(divisao(15,20));
