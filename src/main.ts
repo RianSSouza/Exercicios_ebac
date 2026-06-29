4bae8fea4faebf6ef2e88ab4e750db5927d05717
Rian <rianssouza02@gmail.com>
2026-06-29 13:32:21 -0300

    enviando atividade

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+function multiplicacao(num1: number, num2:number):number{
+    return num1 * num2;
+}
+
+const apresentacaoNome = (nome:string) =>{
+    return `Olá ${nome} !`;
+}
+
+console.log(multiplicacao(5,5));
+console.log(apresentacaoNome('Rian'));
+
