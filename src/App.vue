09ecf715df020538b7489f08dbf606af021b2d1d
Rian <rianssouza02@gmail.com>
2026-07-26 14:20:52 -0300

    Criando o componente formulario

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { reactive } from "vue";
+import Formulario from "./components/Formulario.vue";
 
 const estado = reactive({
   num1: 0,
@@ -37,41 +38,9 @@ const calculaResultado = () => {
         <div class="card shadow">
           <div class="card-body">
             <h2 class="text-center mb-4">Calculadora</h2>
-            <form>
-              <div class="mb-3">
-                <label class="form-label">Primeiro numero: </label>
-                <input
-                  type="number"
-                  class="form-control"
-                  v-model="estado.num1"
-                />
-              </div>
-              <div class="mb-2">
-                <select class="form-select" v-model="estado.operacao">
-                  <option value="somar">+</option>
-                  <option value="subtrair">−</option>
-                  <option value="multiplicacao">×</option>
-                  <option value="divisao">/</option>
-                </select>
-              </div>
-              <div class="mb-3">
-                <label class="form-label">Segundo numero: </label>
-                <input
-                  type="number"
-                  class="form-control"
-                  v-model="estado.num2"
-                />
-              </div>
-              <div
-                :class="
-                  estado.num2 === 0 && estado.operacao === 'divisao'
-                    ? 'alert alert-danger text-center'
-                    : 'alert alert-primary text-center'
-                "
-              >
-                <strong>Resultado:</strong> {{ calculaResultado() }}
-              </div>
-            </form>
+            <Formulario 
+              :resultado="calculaResultado()"
+              :estado="estado"/>
           </div>
         </div>
       </div>
---
+++ src/components/Formulario.vue
@@ -0,0 +1,33 @@
+<script setup>
+    const props = defineProps(['resultado', 'estado']);
+</script>
+
+<template>
+  <form>
+    <div class="mb-3">
+      <label class="form-label">Primeiro numero: </label>
+      <input type="number" class="form-control" v-model="props.estado.num1" />
+    </div>
+    <div class="mb-2">
+      <select class="form-select" v-model="estado.operacao">
+        <option value="somar">+</option>
+        <option value="subtrair">−</option>
+        <option value="multiplicacao">×</option>
+        <option value="divisao">/</option>
+      </select>
+    </div>
+    <div class="mb-3">
+      <label class="form-label">Segundo numero: </label>
+      <input type="number" class="form-control" v-model="props.estado.num2" />
+    </div>
+    <div
+      :class="
+        props.estado.num2 === 0 && props.estado.operacao === 'divisao'
+          ? 'alert alert-danger text-center'
+          : 'alert alert-primary text-center'
+      "
+    >
+      <strong>Resultado:</strong> {{ props.resultado }}
+    </div>
+  </form>
+</template>
